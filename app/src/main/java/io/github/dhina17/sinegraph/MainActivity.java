9bcf185cd8f4684a1bc413c7e76151a5fdb0f03c
Dhina17 <dhinalogu@gmail.com>
2021-07-16 15:45:41 +0530

    gradle: Enable data binding

--- app/src/main/java/io/github/dhina17/sinegraph/MainActivity.java
+++ app/src/main/java/io/github/dhina17/sinegraph/MainActivity.java
@@ -1,14 +1,17 @@
 package io.github.dhina17.sinegraph;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
+
+import io.github.dhina17.sinegraph.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
     }
 }
